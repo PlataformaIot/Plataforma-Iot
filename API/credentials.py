4a9bb45d26e0332b49e7903f5e18705ff25738e0
THETNT\User <renatotbreis@gmail.com>
2021-03-31 10:57:26 -0300

    Remoção de Informações Protegidas
    -> branch "api-dev-build" removida completamente por conter informações protegidas;
    -> remoção de arquivos que estavam inutilizados;
    -> adicionado um arquivo de credenciais.

---
+++ API/credentials.py
@@ -0,0 +1,5 @@
+# insira as credenciais aqui conforme as instruções:
+
+mongodb_address = # endereço do banco de dados no mongodb
+database = # nome do banco de dados a ser acessado
+everynet_login = # payload de login e senha usado na everynet. use este formato: '{"email": EMAIL, "password": PASSWORD}'
