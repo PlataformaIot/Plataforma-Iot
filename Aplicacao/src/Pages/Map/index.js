361b89c67a32055dda90888342223bea4a4898a9
Arley Souto <macx15@hotmail.com>
2021-04-14 15:31:52 -0300

    Iniciado filtro do mapa

--- Aplicacao/src/Pages/Map/index.js
+++ Aplicacao/src/Pages/Map/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Form, Col, Row } from 'react-bootstrap';
+import { Form, Col, Row, Button } from 'react-bootstrap';
+import {FiSearch} from 'react-icons/fi';
 import './styles.css';
 
 export default function Mapa() {
@@ -18,10 +19,16 @@ export default function Mapa() {
                         <option>2</option>
                         <option>3</option>
                     </Form.Control>
+                    <Form.Control style={{ width: 200, marginLeft: '2%' }} as="select">
+                        <option>ano</option>
+                        <option>2</option>
+                        <option>3</option>
+                    </Form.Control>
+                    <Button style={{marginLeft:30,alignItems:'center', display:'flex',}} variant="success"><FiSearch size={20}/>Buscar</Button>
                 </Col>
             </Row>
                 <div className="mapStyle">
-                    <iframe src="http://161.97.133.47:5000/day" width="800" height="400" />
+                    <iframe src={`http://161.97.133.47:5000/day`} width="1150" height="450" />
                 </div>
         </div>
 
