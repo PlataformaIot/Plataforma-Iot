45fa4e1bbdee4a80e7cbb9a9384f4b84932f8953
Daniel Pinheiro <danielborgespinheiro@outlook.com>
2021-04-24 20:26:56 -0300

    Exibição dinâmica campos de variáveis implementada

--- Aplicacao/src/Pages/Home/index.js
+++ Aplicacao/src/Pages/Home/index.js
@@ -7,6 +7,8 @@ import Mapa from '../Map';
 import { GetDadosDevice} from '../../store/functions'
 
 
+const maiorVbat=3.7, menorVbat=2.5;
+
 export default function Home() {
     const {device,dadosDevice,propsDevice,selectedDevice} = GetDadosDevice()
 
@@ -15,7 +17,7 @@ export default function Home() {
 
     function verificaLista(lista,prop){
          const selDevice = (lista.length > 0) ? (selectedDevice === '' ? lista[0].device : lista.filter((dev) => dev.device === selectedDevice)) : []
-         return (selDevice.length > 0) ? selDevice[0][prop] : ""
+         return (selDevice.length > 0) & selDevice[0][prop] ? selDevice[0][prop] : 0.0
     }
 
     return (
@@ -26,88 +28,64 @@ export default function Home() {
                         (dadosDevice.length > 0) ?
                         (
                             dadosDevice.filter((device) => device.device === selectedDevice)[0] ?
-                                dadosDevice.filter((device) => device.device === selectedDevice)[0].type === "temp" ?
-                                    drawInfoPanel3() : drawInfoPanel1() : ""
+                                    drawInfoPanel() : ""
                         ) : ""
                     }
                 </Col>
             </Row>
         </Container >
     )
-    
-    function drawInfoPanel1(){
-        return (
-            <div>
-            <Jumbotron>
 
-                <Col>
-                    <h2 style={{ textAlign: 'center', marginBottom: '2%'  }}>Informações do Dispositivo</h2>
-                    
-                    <CardDeck style={{ textAlign: 'center', marginBottom: '2%' }}>
-                    <Container >
-                    <Row>
-                        <Col sm={12} style={{ marginLeft: '290px'}}>  
-                        <Card bg="success" text='light' >
-                            <Card.Body>
-                                <Card.Title>Tensão da Bateria</Card.Title>
-                                <Card.Text>
-                                    <h1><strong>{verificaLista(dadosDevice,"bateria")}V</strong></h1>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        </Col>
-                        </Row>
-                    </Container>
-                    </CardDeck>
-                   
-                </Col>
-            </Jumbotron>
-
-            <Jumbotron>
-                <Col style={{ textAlign: 'center', marginBottom: '2%' }}>
-                    <h2 style={{ textAlign: 'center', marginBottom: '2%' }}>Mapa</h2>
-                    <Card>
-                        <Card.Body>
-                            <Mapa />
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Jumbotron>
-        </div>
-        )
-    }
-
-    function drawInfoPanel3(){
+    function drawInfoPanel(){
+        var cargaBateria = verificaLista(dadosDevice,"bateria") - menorVbat;
+        var cargaBateria = 100 * cargaBateria/(maiorVbat-menorVbat);
+        if(cargaBateria>100) cargaBateria=100;
+        
         return (
             <div>
             <Jumbotron>
                 <h2 style={{ textAlign: 'center' }}>Informações do Dispositivo</h2>
                 <CardDeck style={{ marginBottom: '2%' }}>
-                    <Card bg="success" text='light'>
-                        <Card.Body>
-                            <Card.Title>Tensão da Bateria</Card.Title>
-                            <Card.Text>
-                                <h1><strong>{verificaLista(dadosDevice,"bateria")}V</strong></h1>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card bg="danger" text='light'>
-                        <Card.Body>
-                            <Card.Title>Temperatura</Card.Title>
-                            <Card.Text>
-                                <h1><strong>{verificaLista(dadosDevice,"temp").toFixed(2)}°C</strong></h1>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card bg="light" text='black'>
-                        <Card.Body>
-                            <Card.Title>Umidade</Card.Title>
-                            <Card.Text>
-                                <h1><strong>{verificaLista(dadosDevice,"hum").toFixed(2)}%</strong></h1>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </CardDeck>
+                    { propsDevice.includes('bateria') ? (
+                        <Card bg="success" text='light'>
+                            <Card.Body>
+                                <Card.Title>Carga da Bateria</Card.Title>
+                                <Card.Text>
+                                    <h1><strong>{cargaBateria.toFixed(0)}%</strong></h1>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    ) : ""}
+                    { propsDevice.includes('temp') ? (
+                        <Card bg="danger" text='light'>
+                            <Card.Body>
+                                <Card.Title>Temperatura</Card.Title>
+                                <Card.Text>
+                                    <h1><strong>{verificaLista(dadosDevice,"temp").toFixed(2)}°C</strong></h1>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    ) : ""}
+                    { propsDevice.includes('hum') ? (
+                        <Card bg="light" text='black'>
+                            <Card.Body>
+                                <Card.Title>Umidade</Card.Title>
+                                <Card.Text>
+                                    <h1><strong>{verificaLista(dadosDevice,"hum").toFixed(2)}%</strong></h1>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    ) : ""}
+                    { propsDevice.includes('velocidade') ? (
+                        <Card bg="warning" text='black'>
+                            <Card.Body>
+                                <Card.Title>Umidade</Card.Title>
+                                <Card.Text>
+                                    <h1><strong>{verificaLista(dadosDevice,"velocidade")}km/h</strong></h1>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    ) : ""}                </CardDeck>
             </Jumbotron>
             <Jumbotron>
                 <Col lg="12" style={{ marginBottom: '5%' }}>
@@ -119,6 +97,20 @@ export default function Home() {
                     </Card>
                 </Col>
             </Jumbotron>
+
+            { (propsDevice.includes('lat') || propsDevice.includes('long')) ? (           
+                <Jumbotron>
+                    <Col style={{ textAlign: 'center', marginBottom: '2%' }}>
+                        <h2 style={{ textAlign: 'center', marginBottom: '2%' }}>Mapa</h2>
+                        <Card>
+                            <Card.Body>
+                                <Mapa />
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Jumbotron>
+            ) : ""}
+            
         </div>
         )
     }
